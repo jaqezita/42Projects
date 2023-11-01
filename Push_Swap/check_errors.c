60ae201d72459c1d51c2229df528e4330378a22f
Jaqueline Ribeiro Inocencio <jaqribei@student.42sp.org.br>
2023-11-01 06:42:34 -0300

    feat: push_swap

--- Push_Swap/check_errors.c
+++ Push_Swap/check_errors.c
@@ -6,7 +6,7 @@
 /*   By: jaqribei <jaqribei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 00:58:55 by jaqribei          #+#    #+#             */
-/*   Updated: 2023/11/01 01:09:26 by jaqribei         ###   ########.fr       */
+/*   Updated: 2023/11/01 02:13:44 by jaqribei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,19 @@ void	check_errors(int argc, char *argv[])
 	int	j;
 
 	if (argc < 2 || argv == NULL)
-		exit(ft_printf("Error\n"));
+		exit(0);
 	i = 1;
 	while (i < argc)
 	{
 		j = i + 1;
 		while (j < argc)
 		{
+			if (!ft_isdigit(argv[i][0]) && argv[i][0] != '-')
+				exit(ft_printf("Error\nInvalid argument.\n"));
 			if (ft_atoi(argv[i]) == ft_atoi(argv[j]))
 				exit(ft_printf("Error\nDuplicate arguments detected.\n"));
+			if (ft_atoi(argv[i]) > 2147483647 || ft_atoi(argv[i]) < -2147483648)
+				exit(ft_printf("Error\nInteger overflow.\n"));
 			j++;
 		}
 		i++;
