89dd19d94328c19c808f5896a5b47c3c63294415
Jaqueline Ribeiro Inocencio <jaqribei@student.42sp.org.br>
2023-12-01 14:36:26 -0300

    exam

--- exam-pratice/level-1/ft_strcpy.c
+++ exam-pratice/level-1/ft_strcpy.c
@@ -1,4 +1,30 @@
-char	*ft_strcpy(char *s1, char *s2);
+char	*ft_strcpy(char *s1, char *s2)
 {
+	int	index;
 
+	index = 0;
+	while (s2[index] != '\0')
+	{
+		s1[index] = s2[index];
+		index++;
+	}
+	s1[index] = '\0';
+	return (s1);
 }
+
+/* #include <stdio.h>
+
+char	*ft_strcpy(char *dest, char *src);
+
+int	main(void)
+{
+	char	dest[255];
+
+	printf("%s\n", ft_strcpy(dest, "Blackpink In Your Area"));
+	printf("%s\n", ft_strcpy(dest, "Jaqueline"));
+	printf("%s\n", ft_strcpy(dest, "1 2 3 4 5 6 7 8 9"));
+	printf("%s\n", ft_strcpy(dest, "Hell0, W0lrd!"));
+	printf("%s\n", ft_strcpy(dest, ""));
+	printf("%s\n", ft_strcpy(dest, "   "));
+	return (0);
+} */
