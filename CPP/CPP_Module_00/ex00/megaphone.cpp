df87113f635507ce8f6017e33fdb0d1dd8f0714a
Jaqueline Ribeiro Inocencio <jaqribei@student.42sp.org.br>
2024-05-08 16:08:01 -0300

    update ex00

---
+++ CPP/CPP_Module_00/ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jaqribei <jaqribei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 15:29:41 by jaqribei          #+#    #+#             */
+/*   Updated: 2024/05/08 15:58:20 by jaqribei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+
+int	main(int argc, char** argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int i = 1; argv[i]; i++)
+	{
+		std::string str(argv[i]);
+		for (size_t j = 0; j < str.size(); j++)
+			str[j] = std::toupper(str[j]);
+		std::cout << str << " ";
+	}
+	std::cout << std::endl;
+	return (0);
+}
